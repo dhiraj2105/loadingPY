bbe4b005d6ce7b7717f602bbfb69a15e28dd6408
dhiraj2105 <47687648+dhiraj2105@users.noreply.github.com>
2022-01-16 20:28:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import time
-import os
+from os import system , name
 
 #animation=[ "10%" , "20%" , "30%" , "40%" , "50%" , "60%" , "70%" , "80%" , "90%" , "100%" ]
 animation= ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
@@ -9,7 +9,12 @@ for i in range(len(animation)):
     sys.stdout.write("\r" + animation[i % len(animation)])
     sys.stdout.flush()
 #to clear screen
-clear = lambda: os.system('cls')
+# clear = lambda: os.system('cls')
+def clear():
+    if name=='nt':
+        _=system('cls')
+    else:
+        _=system('clear')
 time.sleep(1)
 clear()
 print("Done!")
